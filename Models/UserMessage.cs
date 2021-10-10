a7d545b5294f6dd1322b5009046a8ad804adf1c5
Iulian Oana <iulian.andreioana@gmail.com>
2021-10-10 10:12:21 +0100

    Added tutorial files

---
+++ Models/UserMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chatty.Models
+{
+    public class UserMessage
+    {
+        public string Username { get; set; }
+        public string Message { get; set; }
+        public bool CurrentUser { get; set; }
+        public DateTime DateSent { get; set; }
+    }
+}
